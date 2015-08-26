60d9c6360bda2047e4adc006b3d3ad8ada6e568a
Jordi Pamies <jordipamies2@gmail.com>
2015-08-26 10:35:24 +0100

    Conectada arquitectura de estilos separando por paginas

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var num = 600; //number of pixels before modifying styles
-
-      $(window).bind('scroll', function () {
-          if ($(window).scrollTop() > num) {
-              $('.boton').addClass('arriba');
-          } else {
-              $('.boton').removeClass('arriba');
-          }
-      });
