2e86b82d1e9b1a75f018bbee8fbc67feeaf6be31
Jordi Pamies <jordipamies2@gmail.com>
2015-08-30 09:18:26 +0100

    ideas de palabras

--- static/js/main.js
+++ static/js/main.js
@@ -7,3 +7,19 @@ var num = 600; //number of pixels before modifying styles
               $('.boton').removeClass('arriba');
           }
       });
+
+
+
+// $(document).ready(function(){
+
+// $(window).resize(function(){
+// 	  if(window.innerWidth>600){
+// 	    $('.few-tags').removeClass('iconography__module--horizontal');
+// 	    $('.few-tags').addClass('iconography__module--vertical')
+// 	  } else if(window.innerWidth<600){
+// 	    $('.few-tags').removeClass('iconography__module--vertical');
+// 	    $('.few-tags').addClass('iconography__module--horizontal')
+// 	  }
+// 	});
+
+// }
